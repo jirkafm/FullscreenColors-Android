f8b236fffa651c6b1a8ba9b1490aa2a804648d35
Jiri Kaplan <jiri.kaplan@quest.com>
2019-08-28 08:46:00 +0200

    Initial commit

---
+++ app/src/main/java/com/github/jirkafm/fullscreencolors/FullscreenActivity.kt
@@ -0,0 +1,121 @@
+package com.github.jirkafm.fullscreencolors
+
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_fullscreen.*
+import kotlin.random.Random
+
+class FullscreenActivity : AppCompatActivity() {
+
+    private var mVisible: Boolean = false
+    private val rgbMaxValue = 256
+    private val mHideHandler = Handler()
+
+    private val mHidePart2Runnable = Runnable {
+        // Delayed removal of status and navigation bar
+
+        // Note that some of these constants are new as of API 16 (Jelly Bean)
+        // and API 19 (KitKat). It is safe to use them, as they are inlined
+        // at compile-time and do nothing on earlier devices.
+        fullscreen_content.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
+
+    private val mShowPart2Runnable = Runnable {
+        // Delayed display of UI elements
+        supportActionBar?.show()
+        fullscreen_content_controls.visibility = View.VISIBLE
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_fullscreen)
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
+        // Set up the user interaction to manually show or hide the system UI.
+        fullscreen_content.setOnClickListener { changeColor() }
+        // fullscreen_content.setOnLongClickListener(mToggleShowUILongClickListener)
+        fullscreen_content.setOnLongClickListener{ toogleUI() }
+
+        hide()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_fullscreen, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun toogleUI() : Boolean {
+        if (mVisible) {
+            hide()
+        } else {
+            show()
+        }
+
+        return false
+    }
+
+    private fun changeColor() {
+        val rnd = Random.Default
+        val newColor = Color.rgb(rnd.nextInt(rgbMaxValue), rnd.nextInt(rgbMaxValue) ,rnd.nextInt(rgbMaxValue))
+        fullscreen_content.setBackgroundColor(newColor)
+    }
+
+    private fun hide() {
+        // Hide UI first
+        supportActionBar?.hide()
+        fullscreen_content_controls.visibility = View.GONE
+        mVisible = false
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable)
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    private fun show() {
+        // Show the system bar
+        fullscreen_content.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        mVisible = true
+
+        // Schedule a runnable to display UI elements after a delay
+        mHideHandler.removeCallbacks(mHidePart2Runnable)
+        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    fun onQuitAction(menuItem: MenuItem) {
+        finishAffinity()
+    }
+
+    companion object {
+        /**
+         * Whether or not the system UI should be auto-hidden after
+         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
+         */
+        private const val AUTO_HIDE = true
+
+        /**
+         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
+         * user interaction before hiding the system UI.
+         */
+        private const val AUTO_HIDE_DELAY_MILLIS = 3000
+
+        /**
+         * Some older devices needs a small delay between UI widget updates
+         * and a change of the status and navigation bar.
+         */
+        private const val UI_ANIMATION_DELAY = 300
+    }
+}
